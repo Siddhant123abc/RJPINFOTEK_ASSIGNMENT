849a0b4ec545ea2f2a800f7d3cb5ea263d884af4
Siddhant123abc <siddhantsahoo23@gmail.com>
2025-04-16 07:54:39 +0530

    push editviewdeletearticle

--- src/test/java/testRunner/CreateEditViewDeleteArticle_TestRunner.java
+++ src/test/java/testRunner/CreateEditViewDeleteArticle_TestRunner.java
@@ -1,7 +1,11 @@
 package testRunner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
+@CucumberOptions(features= {""},glue= {""},monochrome=true,
+plugin={"pretty","html:target/Reports/htmlReport.html","usage:target/Reports/usagereport","json:target/Reports/JsonReport.json",
+"rerun:target/Failed/failedfile.txt","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class CreateEditViewDeleteArticle_TestRunner extends AbstractTestNGCucumberTests{
 	
 	
